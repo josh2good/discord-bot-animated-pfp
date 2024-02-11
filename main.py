a99134751f317a7afad1884b264417138d9b1c43
Josh <157983939+fymjoshh@users.noreply.github.com>
2024-02-11 10:18:26 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import aiohttp
 client = discord.Client(intents=discord.Intents.default())
 TOKEN = ''
 
-new_profile_pic_url = 'https://i.ibb.co' # use this host upload an image go to the link copy as image address and paste it here
+new_profile_pic_url = 'https://i.ibb.co' 
+# use this host upload an image go to the link copy as image address and paste it here
 
 
 @client.event
